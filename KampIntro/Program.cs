042b0ac2c02d1c3d189e6af1bb63811785395e3e
ÖMER BİLEN <omer.bilen44@hotmail.com>
2021-01-17 01:17:37 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TekrarKampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış oku");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış oku");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi oku");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+        }
+    }
+}
