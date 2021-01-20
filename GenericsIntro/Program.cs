af6436592ae0d3e0d31c9d0d32b38b76ffb9a0fc
ÖMER BİLEN <omer.bilen44@hotmail.com>
2021-01-20 23:50:34 +0300

    20 Ocak Çarşamba Ders Sonu...

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Ömer");
+
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
