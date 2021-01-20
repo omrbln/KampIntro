aa8ea173dc587d243972e77de5f12f587751b9cb
ÖMER BİLEN <omer.bilen44@hotmail.com>
2021-01-21 00:01:25 +0300

    20 Ocak Çarşamba Ders Sonu...

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -16,12 +16,22 @@ namespace GenericsIntro
         {
             T[] tempArray = items;
             items = new T[items.Length+1];
-            for (int i = 0; i < tempArray; i++)
+            for (int i = 0; i < tempArray.Length; i++)
             {
                 items[i] = tempArray[i];
             }
 
             items[items.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Items
+        {
+            get { return items; }
         }
     }
 }
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -8,11 +8,21 @@ namespace GenericsIntro
         {
             
             MyList<string> isimler = new MyList<string>();
-            isimler.Add("Ömer");
+            isimler.Add("Engin");
 
-            
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
 
             Console.WriteLine("Hello World!");
+
         }
     }
 }
